b46e18b97c10c1ba51e628978d173613871e8405
Timothee Regis <registimothee@gmail.com>
2020-02-25 12:40:02 +0100

    done merging changes

--- app/controllers/sport_sessions_controller.rb
+++ app/controllers/sport_sessions_controller.rb
@@ -2,5 +2,12 @@ class SportSessionsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
   def index
     @sport_sessions = SportSession.all
+    @markers = @sport_sessions.map do |sport_session|
+      {
+        lat: sport_session.latitude,
+        lng: sport_session.longitude,
+        infoWindow: render_to_string(partial: "infowindow", locals: { sport_session: sport_session }),
+        image_url: helpers.asset_url('old-walker.png')
+      }
   end
 end
