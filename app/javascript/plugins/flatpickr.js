22b449724e55f08fc587c27b62924c513dbe6e26
Emmanuel ROSE <roseemmanuel@hotmail.com>
2020-02-28 14:47:00 +0100

    return

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -2,15 +2,13 @@ import flatpickr from "flatpickr";
 
 
 const initFlatpickr = () => {
-  const input = document.querySelector(".datepickr");
-  if (input) {
-    flatpickr(input, {
-      altInput: true,
-      altFormat: "F j, Y",
-      dateFormat: "Y-m-d",
-      minDate: "today",
-    });
-  };
+  const input = document.querySelector(".datepickr")
+  flatpickr(input, {
+    altInput: true,
+    altFormat: "F j, Y",
+    dateFormat: "Y-m-d",
+    minDate: "today",
+  });
 };
 
 export { initFlatpickr };
