547f24633213287ae8e8f50f2dc6b2765d940d2e
renaud dor <renaud.dor@gmail.com>
2020-03-04 11:35:53 +0100

    add hp background video + add icon SVG + change navbar with text logo and logo.svg on HP + add new SVG helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,10 @@ module ApplicationHelper
       count == 1 ? "#{noun}#{text}" : "#{noun.pluralize}#{text}"
     end
   end
+
+def show_svg(path)
+  File.open("app/assets/images/#{path}", "rb") do |file|
+    raw file.read
+  end
+end
 end
