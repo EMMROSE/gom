d47c293f836a0379df259282c5e22c3f068eaae4
Timothee Regis <registimothee@gmail.com>
2020-03-09 15:50:16 +0100

    added validation photo and username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :login_new
   before_action :notifications_generation
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :default_username
   private
 
   def login_new
